255913bb7f84bb05e161f047cabafc99e49f9f4b
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-08-29 14:21:13 +0800

    Converted locationStore to use composition

--- src/stores/locationStore.ts
+++ src/stores/locationStore.ts
@@ -1,25 +1,49 @@
 import { defineStore } from "pinia";
 
 import { useStorage } from "@vueuse/core";
-import type { resourceInterface } from "./resourceStore";
+import IsledleData from "@/config/IsledleData.json";
+import type { DropTableInterface } from "./dropTableStore";
+import { MessageBus } from "@/models/MessageBus";
+import { ref } from "vue";
+
 
 export interface LocationInterface{
+    "name": string,
+    "unlocked": boolean,
     "specialInteractName": string,
-    "gatherDropTable": resourceInterface["name"][],
-    "specialInteractDropTable": resourceInterface["name"][],
+    "dropTable": DropTableInterface['id']
+    "specialDropTable": DropTableInterface['id'],
 }
 
-export const useLocationStore = defineStore({
-  id: "location",
-  state: () => ({
 
-  }),
 
-  getters: {
+export const useLocationStore = defineStore('location', () => {
+  const dataName = "locations"
+  const data = ref(useStorage("locationData", IsledleData[dataName] as { [key: string]: LocationInterface }));
+  const messageBus = new MessageBus({
 
-  },
+  });
+
+
+  // Developer Functions
+  function addLocation( newLocation: LocationInterface) {
+    data.value[newLocation.name] = newLocation
+  }
+
+  function deleteEntry( name: LocationInterface['name'] ){
+    console.log(name)
+    delete data.value[name]
+  }
   
-  actions: {
 
-  },
+  function resetData(){
+    data.value = IsledleData[dataName] as { [key: string]: LocationInterface }
+  }
+
+  return { resetData,
+    deleteEntry,
+    addLocation,
+    data,
+    dataName,
+  }
 });
